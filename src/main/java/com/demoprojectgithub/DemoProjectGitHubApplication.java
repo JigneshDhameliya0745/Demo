518e5ab134edb84e7eff2b03b6699620a7dfdee1
JigneshDhameliya0745 <dhameliyajignesh214@gmail.com>
2022-04-20 19:21:07 +0530

    Initial Commit

---
+++ src/main/java/com/demoprojectgithub/DemoProjectGitHubApplication.java
@@ -0,0 +1,13 @@
+package com.demoprojectgithub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoProjectGitHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoProjectGitHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demoprojectgithub/DemoProjectGitHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demoprojectgithub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoProjectGitHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
